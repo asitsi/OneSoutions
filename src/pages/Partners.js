529a5c07f90affd187ebdc2771b1a1f42326dc4c
Asitsingh6394066616 <asitsingh0@gmail.com>
2023-06-04 00:19:47 +0530

    public folder remove

--- src/pages/Partners.js
+++ src/pages/Partners.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import Container from '../components/container';
+import Stripe from '../components/Partners/stripe';
 
 const Partners = () => {
   return (
